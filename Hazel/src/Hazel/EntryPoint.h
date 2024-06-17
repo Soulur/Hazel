208efd9434f868e332de6b156ac996f5011f2ff4
Soulur <107337595+Soulur@users.noreply.github.com>
2024-06-17 19:21:24 +0800

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel/EntryPoint.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+#ifdef HZ_PLATFORM_WINDOWS
+
+extern Hazel::Application* Hazel::CreatApplication();
+
+int main(int argc , char** argv)
+{
+	auto app = Hazel::CreatApplication();
+	app->Run();
+	delete app;
+}
+
+#else
+	#error Hazel only support Window!
+#endif // HZ_PLATFORM_WINDOWS
---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,22 @@
+#include <Hazel.h>
+
+class Sandbox : public Hazel::Application
+{
+public:
+	Sandbox()
+	{
+
+	}
+
+	~Sandbox()
+	{
+
+	}
+
+};
+
+
+Hazel::Application* Hazel::CreatApplication()
+{
+	return new Sandbox();
+}
