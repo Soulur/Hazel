e2c4aeaf1052c290f11ea2221844a8c296780d7d
Soulur <107337595+Soulur@users.noreply.github.com>
2024-08-30 21:21:48 +0800

    Rendering Interface API

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -10,7 +10,8 @@
 
 #include "ImGui/ImGuiLayer.h"
 
-#include "Hazel/Render/Shader.h"
+#include "Hazel/Renderer/Shader.h"
+#include "Hazel/Renderer/Buffer.h"
 
 namespace Hazel {
 
@@ -38,9 +39,11 @@ namespace Hazel {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		unsigned int m_VertexArray;
 
 		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 	private:
 		static Application* s_Instace;
 	};
