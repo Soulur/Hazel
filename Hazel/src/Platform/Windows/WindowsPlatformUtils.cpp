b1391f937adb7f03e2665b5cb4fe29838989551f
Soulur <3054353031@qq.com>
2024-10-16 15:48:30 +0800

    Removed redundant include in WindowsPlatformUtils.cpp file

--- Hazel/src/Platform/Windows/WindowsPlatformUtils.cpp
+++ Hazel/src/Platform/Windows/WindowsPlatformUtils.cpp
@@ -1,7 +1,6 @@
 #include "hzpch.h"
 #include "Hazel/Utils/PlatformUtils.h"
 
-#include <sstream>
 #include <commdlg.h>
 #include <GLFW/glfw3.h>
 #define GLFW_EXPOSE_NATIVE_WIN32
@@ -26,6 +25,7 @@ namespace Hazel {
 		ofn.lpstrFilter = filer;
 		ofn.nFilterIndex = 1;
 		ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
+		
 		if (GetOpenFileNameA(&ofn) == TRUE)
 			return ofn.lpstrFile;
 		
