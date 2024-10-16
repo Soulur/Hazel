956e81c78f47939c47124fb4175000cade1db382
Soulur <3054353031@qq.com>
2024-10-16 20:42:41 +0800

    Refactored framebuffers to allow for MRTs

--- Hazelnut/src/EditorLayer.cpp
+++ Hazelnut/src/EditorLayer.cpp
@@ -227,7 +227,7 @@ namespace Hazel {
             ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
             m_ViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
 
-            uint64_t textureID = m_Framebuffer->GetColorAttachmentRendererID();
+            uint64_t textureID = m_Framebuffer->GetColorAttachmentRendererID(1);
             ImGui::Image(reinterpret_cast<void*>(textureID), ImVec2{m_ViewportSize.x , m_ViewportSize.y}, ImVec2{0 , 1}, ImVec2{1 , 0});
 
             // Gizmos
